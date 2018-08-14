b72b23151f44ae229fa87f95184e643840f1bce8
MarimeGui <lepro.guillaume@gmail.com>
2018-08-14 18:30:21 +0200

    Proper Errors, example MIDI Frequency Lookup

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,80 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug)]
+pub enum SynthesizerError {
+    TimeInvalid(TimeInvalidError),
+    NoFrequencyForID(NoFrequencyForIDError),
+}
+
+impl Error for SynthesizerError {
+    fn description(&self) -> &str {
+        match *self {
+            SynthesizerError::TimeInvalid(ref e) => e.description(),
+            SynthesizerError::NoFrequencyForID(ref e) => e.description(),
+        }
+    }
+}
+
+impl Display for SynthesizerError {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            SynthesizerError::TimeInvalid(ref e) => e.fmt(f),
+            SynthesizerError::NoFrequencyForID(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl From<TimeInvalidError> for SynthesizerError {
+    fn from(e: TimeInvalidError) -> SynthesizerError {
+        SynthesizerError::TimeInvalid(e)
+    }
+}
+
+impl From<NoFrequencyForIDError> for SynthesizerError {
+    fn from(e: NoFrequencyForIDError) -> SynthesizerError {
+        SynthesizerError::NoFrequencyForID(e)
+    }
+}
+
 #[derive(Debug)]
 pub struct TimeInvalidError {
     pub value: f64,
 }
 
+impl Error for TimeInvalidError {
+    fn description(&self) -> &str {
+        "A provided value cannot be interpreted as a valid Time, Frequency or Duration"
+    }
+}
+
+impl Display for TimeInvalidError {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "Wrong value: {}",
+            self.value
+        )
+    }
+}
+
 #[derive(Debug)]
 pub struct NoFrequencyForIDError {
     pub id: usize,
+}
+
+impl Error for NoFrequencyForIDError {
+    fn description(&self) -> &str {
+        "The Frequency Lookup failed, the provided IS was invalid"
+    }
+}
+
+impl Display for NoFrequencyForIDError {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "Wrong ID: {}",
+            self.id
+        )
+    }
 }
--- src/flut.rs
+++ src/flut.rs
@@ -1,4 +1,4 @@
-use error::NoFrequencyForIDError;
+use error::{SynthesizerError, NoFrequencyForIDError};
 use std::collections::HashMap;
 use std::hash::BuildHasher;
 use std::result::Result;
@@ -7,14 +7,23 @@ use util::Frequency;
 /// Provides a Frequency value from a Frequency ID
 pub trait FrequencyLookup {
     /// The function that does the link between the ID and the Frequency
-    fn get_freq(&mut self, id: &usize) -> Result<Frequency, NoFrequencyForIDError>;
+    fn get_freq(&mut self, id: &usize) -> Result<Frequency, SynthesizerError>;
 }
 
 impl<S: BuildHasher> FrequencyLookup for HashMap<usize, Frequency, S> {
-    fn get_freq(&mut self, id: &usize) -> Result<Frequency, NoFrequencyForIDError> {
+    fn get_freq(&mut self, id: &usize) -> Result<Frequency, SynthesizerError> {
         match self.get(id) {
             Some(f) => Ok(*f),
-            None => Err(NoFrequencyForIDError { id: *id }),
+            None => Err(SynthesizerError::NoFrequencyForID(NoFrequencyForIDError{id: *id,})),
         }
+    }
+}
+
+/// Example implementation of FrequencyLookup for MIDI
+pub struct MIDIFrequencyLookup {}
+
+impl FrequencyLookup for MIDIFrequencyLookup {
+    fn get_freq(&mut self, id: &usize) -> Result<Frequency, SynthesizerError> {
+        Ok(Frequency::new(2f64.powf((id - 69) as f64 / 12f64) * 440f64)?)
     }
 }
